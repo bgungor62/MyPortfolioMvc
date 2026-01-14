1ac81b6c4be07b1732c62493e0b692035fcacfbd
Baran Güngör <110722066+bgungor62@users.noreply.github.com>
2026-01-14 17:12:22 +0300

    Project entity yazıldı

---
+++ Models/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace MyPortfolio.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+        public string? ProjectUrl { get; set; }
+        public string? GithubUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}

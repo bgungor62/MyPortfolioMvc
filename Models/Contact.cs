ca6259bc5852bc2f8a3677d09ba34f7d75b0590d
Baran Güngör <110722066+bgungor62@users.noreply.github.com>
2026-01-15 10:31:19 +0300

    Contect ve Testimonial entityyleri yazıldı

---
+++ Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace MyPortfolio.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? Subject { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+
+    }
+}
